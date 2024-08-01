32977475b3749ae02e791e47220f101881ca0415
Dizelars <ars_logachev@mail.ru>
2024-08-01 19:09:33 +0300

    страница для QR-кода + вызов попапа

--- src/js/google_viewer/AR.js
+++ src/js/google_viewer/AR.js
@@ -1,12 +1,76 @@
 document.querySelector('.pulse_wrapper').addEventListener('click', () => {
-    console.log('click AR');
-	const modelViewer = document.querySelector('model-viewer');
-    console.log(modelViewer);
+    // console.log('click AR');
+	const modelViewer = document.querySelector('#pageWithModel');
+    // console.log(modelViewer);
+	// console.log(modelViewer.canActivateAR);
 	if (modelViewer.canActivateAR) {
-        console.log('canActivateAR');
+        // console.log('canActivateAR');
 		modelViewer.activateAR();
-        console.log('AR triggered');
+        // console.log('AR triggered');
 	} else {
 		console.log('Открываем QR с ссылкой на AR');
+		openErrorStatus()
+		// window.open("./pageAR.html");
 	}
 });
+
+// Popip с QR-кодом
+const statusError = document.querySelector('.qrCode-popup');
+const arButton = document.querySelector('.pulse_wrapper');
+
+// Для отключения скролла
+let bodyOverflow = document.querySelector('body');
+let scrollPosition = 0;
+
+function closePopup() {
+	bodyOverflow.style.removeProperty("overflow");
+	bodyOverflow.style.removeProperty("position");
+	bodyOverflow.style.removeProperty("top");
+	bodyOverflow.style.removeProperty("width");
+	window.scrollTo(0, scrollPosition);
+}
+
+function openPopup() {
+	scrollPosition = window.scrollY;
+	bodyOverflow.style.overflow = "hidden";
+	bodyOverflow.style.position = "fixed";
+	bodyOverflow.style.top = `-${scrollPosition}px`;
+	bodyOverflow.style.width = "100%";
+}
+
+function openErrorStatus() {
+	statusError.classList.add('active');
+	statusError.style.visibility = 'visible';
+	arButton.classList.add('hidden');
+	openPopup();
+}
+
+function closeErrorStatus() {
+	statusError.classList.remove('active');
+	arButton.classList.remove('hidden');
+	setTimeout(() => {
+		statusError.style.visibility = 'hidden';
+	}, 450);
+	closePopup();
+}
+
+// function clickOutOfPopup(popup, closeFunc) {
+// 	popup.addEventListener('mousedown', (event) => {
+// 		const target = event.target;
+// 		const isInsidePopup = target.closest('.qrCode-popup_wrapper');
+// 		if (!isInsidePopup) {
+// 			closeFunc();
+// 		}
+// 	});
+// }
+
+// clickOutOfPopup(statusError, closeErrorStatus);
+
+function clickButtonClose(clickedElem, closeFunc) {
+	clickedElem.addEventListener('click', () => {
+		closeFunc();
+	});
+}
+
+const statusErrorCloseIcon = document.querySelector('.qrCode-popup .closeButton');
+clickButtonClose(statusErrorCloseIcon, closeErrorStatus);
