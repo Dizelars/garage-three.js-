951999c5c5195cd83b143ad5ad30eba28af4062d
Dizelars <ars_logachev@mail.ru>
2024-10-18 13:08:52 +0300

    robots, sitemap, pageAR

--- src/js/fullModelScripts/kater.js
+++ src/js/fullModelScripts/kater.js
@@ -1,9 +1,9 @@
 import '../modelPage/page_logic.js';
 
 // Скрипты для three.js
-import '../threeJS/scene_kater.js';
-import '../google_viewer/AR.js';
+// import '../threeJS/scene_kater.js';
+// import '../google_viewer/AR.js';
 
 // Скрипты для предупреждения
-// import '../modelPage/vectary_403.js';
-// import '../modelPage/changeScene360.js';
+import '../modelPage/vectary_403.js';
+import '../modelPage/changeScene360.js';
